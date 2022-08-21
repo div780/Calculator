856d11104ee2104f0021f3743efd70b4eb4c79ee
div780 <divyanshuraj36162@gmail.com>
2022-08-22 01:51:21 +0530

    finished project

---
+++ app/src/main/java/com/div/mycalculator/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.div.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.ArithmeticException
+
+class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? =null
+    var lastNumeric: Boolean=false
+    var lastDot: Boolean=false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvInput=findViewById(R.id.tvInput)
+    }
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text)
+        lastDot=false
+        lastNumeric=true
+    }
+
+    fun onClear(view: View){
+        tvInput?.text=""
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastDot=true
+            lastNumeric=false
+        }
+    }
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastDot=false
+                lastNumeric=false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value:String): Boolean{
+        return if(value.startsWith("-")){
+            false
+        }
+        else{
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix=""
+
+            try {
+                if(tvValue.startsWith("-")){
+                    tvValue=tvValue.substring(1)
+                    prefix="-"
+                }
+                if(tvValue.contains("-")){
+                    val splitValue=tvValue.split("-")
+                    var one =splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=(one.toDouble()-two.toDouble()).toString()
+                }
+                else if(tvValue.contains("+")){
+                    val splitValue=tvValue.split("+")
+                    var one =splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=(one.toDouble()+two.toDouble()).toString()
+                }
+                else if(tvValue.contains("*")){
+                    val splitValue=tvValue.split("*")
+                    var one =splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=(one.toDouble()*two.toDouble()).toString()
+                }
+                else if(tvValue.contains("/")){
+                    val splitValue=tvValue.split("/")
+                    var one =splitValue[0]
+                    var two=splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+                    tvInput?.text=(one.toDouble()/two.toDouble()).toString()
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+}
