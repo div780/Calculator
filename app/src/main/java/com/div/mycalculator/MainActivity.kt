4a1e5493d289d7e0b83359252e1ee62e68a83313
div780 <divyanshuraj36162@gmail.com>
2023-02-06 16:53:37 +0530

    finished project

--- app/src/main/java/com/div/mycalculator/MainActivity.kt
+++ app/src/main/java/com/div/mycalculator/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     var lastDot: Boolean=false
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
